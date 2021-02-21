9a1f425d8c44a86b0139716ad58f4cab7962b735
r0otc0de <66352725+r0otc0de@users.noreply.github.com>
2021-02-21 12:59:13 +0600

    Update main.sh

--- main.sh
+++ main.sh
@@ -20,12 +20,12 @@ sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
 sudo apt -y install obs-studio
 sudo apt -y install firefox
-sudo adduser ALOK chrome-remote-desktop
+sudo adduser arthur chrome-remote-desktop
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
 printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
 read -p "Paste Here: " CRP
-su - ALOK -c """$CRP"""
+su - arthur -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
 printf 'Your SUDO Pasword Is 42069 \n'
